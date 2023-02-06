da96412e91b052ecf46a95af3f66abe7c58634a8
unknown <git@bvch.mx>
2023-02-06 11:38:10 +0100

    Implemented a Piece class to manipulate active pc.

--- tetris.py
+++ tetris.py
@@ -1,3 +1,7 @@
+import math
+import random
+
+# This class describes the Tetris game from an abstract point of view
 class Game:
 	def __init__(self, width=10, height=20) -> None:
 		self.board = list[list[width]]
@@ -41,5 +45,40 @@ class Game:
 			'#3457ff'
 		]
 		"""
+		self.emptyActivePiece()
 
+	def emptyActivePiece(self):
+		self.activePiece = Piece((0,0),[[0]],-1)
+
+	def activePieceIsEmpty(self):
+		return self.activePiece.piece_type == -1
+	
+	def pieceInIllegalPos(self, piece: Piece):
+		pieceInGrid = piece.petrify()
 		pass
+
+	def update(self):
+		if (self.activePieceIsEmpty()):
+			piece_type = random.randrange(0, len(self.pieces))
+			dimension = len(self.pieces[piece_type])
+			start_x = math.floor((self.width - dimension) / 2)
+			start_y = math.ceil(dimension / 2)
+			if (self.pieceInIllegalPos(self.activePiece)):
+				pass
+
+# This class describes a piece in Tetris
+class Piece:
+	def __init__(self, pos: tuple[int,int], structure: list[list[int]], piece_type: int):
+		self.pos = pos
+		self.structure = structure
+		self.piece_type = piece_type
+	
+	def petrify(self):
+		petrifiedStructure: list[list[int]] = []
+		for y in range(len(self.structure)):
+			for x in range(len(self.structure[y])):
+				blockCoord = (x,y)
+				blockValue = self.structure[y][x]
+				if blockValue == 1:
+					petrifiedStructure[y+self.pos[1]][x+self.pos[0]] = 1
+		return petrifiedStructure
